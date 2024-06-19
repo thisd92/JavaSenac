f0f82a701ffa8f5c02fc1e15be2c8e4698b672ad
Thiago Dutra <thiiagodutra@gmail.com>
2024-06-18 21:05:14 -0300

    exericio 18062024

--- src/exercicios/Exercicios.java
+++ src/exercicios/Exercicios.java
@@ -1,26 +1,22 @@
 package exercicios;
 
 import java.util.Scanner;
-import modelo.Modelo;
 
 public class Exercicios {
 
     public static void main(String[] args) {
-        
+
+        int num1, num2, num3, i = 0;
         Scanner scan = new Scanner(System.in);
-        int qtdeNumeros = 0;
-        Modelo modelo = new Modelo();
         
-        
-        System.out.print("Quantos números serão sorteados? ");
-        qtdeNumeros = scan.nextInt();
-        
-        modelo.sorteio(qtdeNumeros);
-        
-        System.out.println("\nNúmeros primos sorteados:");
-        
-        
+        num1 = scan.nextInt();
+        System.out.println("O dobro de " + num1 + " é " + (num1 * 2));
+        num2 = scan.nextInt();
+        System.out.println("O quadrado de " + num2 + " é " + (num2 * num2));
+        num3 = scan.nextInt();
+        System.out.println("O número anterior a " + num3 + " é " + (num3 - 1) + " e o posterior é " + (num3 + 1));
+
         scan.close();
     }
-    
+
 }
